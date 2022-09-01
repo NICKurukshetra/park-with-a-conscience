e3a972b696bd05bed81aa0bed35102399df36f58
Shivam Verma <vermashivams1510@gmail.com>
2022-09-01 10:50:07 +0530

    added more categories

--- lib/data/homeScreenCategories.dart
+++ lib/data/homeScreenCategories.dart
@@ -1,31 +1,35 @@
-import 'package:park_with_conscience/model/home_screen_categories.dart';
-import 'package:park_with_conscience/screens/freeNurseryScreen.dart';
-
-List<Details> categories = [
-  Details(
-    name: "Solar Motion Sensor Lights", 
-    imageAddress: "assets/images/solarLight.jpg",
-    screenName: NurseryScreen()
-    ),    
-  Details(
-    name: "Solar Motion Sensor Lights", 
-    imageAddress: "assets/images/solarLight.jpg",
-    screenName: NurseryScreen()
-    ),    
-  Details(
-    name: "Solar Motion Sensor Lights", 
-    imageAddress: "assets/images/day:night SolarLights.png",
-    screenName: NurseryScreen()
-    ),    
-  Details(
-    name: "Solar Motion Sensor Lights", 
-    imageAddress: "assets/images/day:night SolarLights.png",
-    screenName: NurseryScreen()
-    ),    
-  Details(
-    name: "Solar Motion Sensor Lights", 
-    imageAddress: "assets/images/day:night SolarLights.png",
-    screenName: NurseryScreen()
-    ),    
-
+import 'package:park_with_conscience/model/home_screen_categories.dart'; 
+import 'package:park_with_conscience/screens/freeNurseryScreen.dart'; 
+List<Details> categories = [ 
+  Details( 
+    name: "Smart Solar Lights", 
+    imageAddress: "assets/images/solarLight.jpg", 
+    screenName: NurseryScreen() 
+  ), 
+  Details( 
+    name: "Sprinklers", 
+    imageAddress: "assets/images/solarLight.jpg", 
+    screenName: NurseryScreen() 
+  ), 
+  Details( 
+    name: "Free Nurrery", 
+    imageAddress: "assets/images/day:night SolarLights.png", 
+    screenName: NurseryScreen() 
+  ), 
+  Details( 
+    name: "Entry Gates", 
+    imageAddress: "assets/images/day:night SolarLights.png", 
+    screenName: NurseryScreen() 
+  ), 
+  Details( 
+    name: "Drainage", 
+    imageAddress: "assets/images/day:night SolarLights.png", 
+    screenName: NurseryScreen() 
+  ), 
+  Details( 
+    name: "Compost pit", 
+    imageAddress: "assets/images/day:night SolarLights.png", 
+    screenName: NurseryScreen() 
+  ), 
+  
 ];

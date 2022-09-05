4e3cf03b807ef9a631df0432d65635f1421fd46a
Shivam Verma <vermashivams1510@gmail.com>
2022-09-05 10:10:04 +0530

    modified solar page

--- lib/screens/solarLightsScreen.dart
+++ lib/screens/solarLightsScreen.dart
@@ -2,58 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:shimmer/shimmer.dart';
 
-class SolarLightsScreen extends StatefulWidget {
+class SolarLightsScreen extends StatelessWidget {
   const SolarLightsScreen({Key? key}) : super(key: key);
-
-  @override
-  State<SolarLightsScreen> createState() => _SolarLightsScreenState();
-}
-
-class _SolarLightsScreenState extends State<SolarLightsScreen> {
-  bool isLoading = true;
-  List<String> imageUrls = [
-    "https://drive.google.com/uc?id=10uKmuCaRPC9odfJFMuufLwW2IttCNevQ", 
-    "https://drive.google.com/uc?id=1BbGCVZDt_qQUCBE-uO6zkfvsB8PGmft6", 
-    "https://drive.google.com/uc?id=1iALWE5y7qCcOyKHPh5ga8nSH9m8N7MfB",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-    // "https://cdn.s3waas.gov.in/s3248e844336797ec98478f85e7626de4a/uploads/2022/01/2022011919.jpg",
-
-  ];
-  
-  List<Image> allImages = [];
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    loadImages();
-    
-  }
-
-  Future loadImages() async {
-    Image _image = Image(image: AssetImage("assets/images/istockphoto-1181366400-612x612.jpeg"));
-    allImages.add(_image);
-    for(String url in imageUrls) {
-      _image = Image.network(url.toString(), fit: BoxFit.cover,);
-      allImages.add(_image);
-    }
-    _image.image.resolve(ImageConfiguration())
-    .addListener(ImageStreamListener(
-      (ImageInfo info, bool syncCall) => setState(() {
-        isLoading = false;
-      })));
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,129 +17,53 @@ class _SolarLightsScreenState extends State<SolarLightsScreen> {
           backgroundColor: Colors.blueGrey,
           
         ),
-        body: isLoading 
-        ? Column(
+        body: Column(
           children: [
-            Shimmer.fromColors(
-            child: Container(
-              color:Colors.black, 
-              height: size.height * 0.2, 
-              margin: EdgeInsets.only(top: 10,bottom: 10)), 
-            baseColor: Color.fromARGB(149, 158, 158, 158), 
-            enabled: isLoading,
-            highlightColor: Colors.grey
-          ),
-            Expanded(child: AnimationGridView(isLoading: isLoading)),
-          ],
-        ) 
-        : SingleChildScrollView(
-          child: Column(
-            children: [
-              // Container(height: size.height * 0.2, color: Colors.black,),
-              Container(
-                height: size.height * 0.25,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/solarLights.gif"),
-                    fit: BoxFit.cover
-                    )),
-              ),
-              Container(
+            Container(
+              height: size.height * 0.28,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/solarLights.gif"),
+                  fit: BoxFit.cover
+                  )),
+            ),
+            Expanded(
+              child: Container(
                 decoration: BoxDecoration(
                   gradient: LinearGradient(colors: [
-                Colors.green.withOpacity(.6),
-                Colors.white.withOpacity(.6),
-                // Colors.green.withOpacity(.4),
-                // Color.fromARGB(255, 235, 217, 162).withOpacity(.3),
-              ]
-                ),),
-                //color: Colors.white,
-                padding: EdgeInsets.only(left: 10),
-                margin: EdgeInsets.only(top: 10, bottom: 10),
+                    Colors.green.withOpacity(.6),
+                    Colors.white.withOpacity(.6),
+                 ]),
+                ),
+                padding: EdgeInsets.all(20),
                 child: Text(
-                  "Solar powered outdoor light \n270 degree spread , \n120 degree wide angle sensor, \n15 second timeout after person leaves\nAutomatic Lighting - Light activates only when sensing motion at night.\nThis acts as a security light\nNo electricity required,\nNo Wire Needed,\nThe light lasts for 8-10 hours if fully charged \n1800 watt battery.",
-                  style: Theme.of(context).textTheme.bodySmall!
-                  .copyWith(
+                  """\u2022 Solar powered outdoor light \n
+\u2022 270 degree spread \n
+\u2022 120 degree wide angle sensor \n
+\u2022 15 second timeout after person leaves \n
+\u2022 Automatic Lighting - Light activates only when sensing motion at night.\n
+\u2022 This acts as a security light \n
+\u2022 No electricity required \n
+\u2022 No Wire Needed \n
+\u2022 The light lasts for 8-10 hours if fully charged \n
+\u2022 1800 watt battery.""",
+                  style: TextStyle(
                     color: Colors.black,
                     fontWeight: FontWeight.bold,
                     fontStyle: FontStyle.italic,
-                    fontSize: 18
+                    fontSize: 18,
+
                   )
                 ), 
                 height: size.height * 0.4, 
-                // margin: EdgeInsets.only(bottom: 10)
               ),
-              
-              // ImagesGridView(allImages: allImages),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-class ImagesGridView extends StatelessWidget {
-  const ImagesGridView({
-    Key? key,
-    required this.allImages,
-  }) : super(key: key);
-
-  final List<Image> allImages;
-
-  @override
-  Widget build(BuildContext context) {
-    return StaggeredGridView.countBuilder(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      crossAxisCount: 3, 
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 10,
-      itemCount: allImages.length - 1,
-      itemBuilder: ((context, index) {
-        return Container(
-          child: allImages[index + 1],
-        );
-      }
-    ), 
-      staggeredTileBuilder: (index) => StaggeredTile.count(
-         (index % 5 == 0) ? 2 : 1, (index % 5 == 0) ? 2 : 1
-      ),
-
-    );
-  }
-}
-
-class AnimationGridView extends StatelessWidget {
-  const AnimationGridView({
-    Key? key,
-    required this.isLoading,
-  }) : super(key: key);
-
-  final bool isLoading;
-
-  @override
-  Widget build(BuildContext context) {
-    return StaggeredGridView.countBuilder(
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 10,
-      itemCount: 20,
-      crossAxisCount: 3,
-      staggeredTileBuilder: (index) => StaggeredTile.count(
-         (index % 5 == 0) ? 2 : 1, (index % 5 == 0) ? 2 : 1
-      ),
-      itemBuilder: (context, index) {
-        return Shimmer.fromColors(
-          child: Container(color:Colors.black), 
-          baseColor: Color.fromARGB(149, 158, 158, 158), 
-          enabled: isLoading,
-          highlightColor: Colors.grey
-        );
-      },
-    );
-  }
-}
-
 
 
 

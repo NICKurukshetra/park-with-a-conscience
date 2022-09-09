6a847c718f75e664ce5a580efc673e3782692cd5
Shivam Verma <vermashivams1510@gmail.com>
2022-09-09 10:12:12 +0530

    added plant Info category

--- lib/data/homeScreenCategories.dart
+++ lib/data/homeScreenCategories.dart
@@ -1,18 +1,6 @@
 import 'package:park_with_conscience/model/home_screen_categories.dart';
 import 'package:park_with_conscience/screens/categoryScreen.dart';
 List<Details> categories = [ 
-  Details(
-    name: "Plants Information", 
-    imageAddress: "assets/images/plantTile.jpeg", 
-    screenName: CategoryScreen(
-      title: "Plants Information",
-      imagePath: "assets/gifs/plant information.gif",
-      aboutContent: """Plants Information Depiction System
-
-The park is equipped with tiles and
-""",
-    )
-  ),
   Details( 
     name: "Compost pit", 
     imageAddress: "assets/images/compostPit.jpeg", 
@@ -65,6 +53,18 @@ Challenges:
 The sprinklers need to be regularly used else they would become blocked with algae.
 """, 
       ) 
+  ),
+  Details(
+    name: "Plants Information", 
+    imageAddress: "assets/images/plantTile.jpeg", 
+    screenName: CategoryScreen(
+      title: "Plants Information",
+      imagePath: "assets/gifs/plant information.gif",
+      aboutContent: """Plants Information Depiction System
+
+The park is equipped with tiles and
+""",
+    )
   ),
   Details( 
     name: "Automatic Closing gates", 

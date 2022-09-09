c744a3d2d44b58ea9bd649f467905e1d001f722a
Shivam Verma <vermashivams1510@gmail.com>
2022-09-09 10:12:43 +0530

    updated drawer to be scrollable

--- lib/data/drawerCategories.dart
+++ lib/data/drawerCategories.dart
@@ -1,5 +1,21 @@
 const allDrawerImages = [
   [
+    
+    "https://drive.google.com/uc?id=1bUKq1Aa7ldkvThwQ0kD2c-I19aTd-CNA", 
+    "https://drive.google.com/uc?id=1gqWOjy7mdI-85-uAhzokK_efNZWEb8M0", 
+    "https://drive.google.com/uc?id=1kPIGla1iBGFnX7kjdUSkCm5Ei-VkBPGk", 
+    "https://drive.google.com/uc?id=1lxxHhBbya75BLS0oSZznrVqdGrSigDQL", 
+    "https://drive.google.com/uc?id=1m2Um8j88HEPz71eQmm4xqr0rHLGQleiP", 
+    "https://drive.google.com/uc?id=1nnVnPu-y8dxZr1vPre5IB3PNXpVwqjZI", 
+    "https://drive.google.com/uc?id=1ovkVV9Vgv9Dr8d1hcvcQisAudmym2kQ8", 
+    "https://drive.google.com/uc?id=1pZ2f6OZX1p50oR14tyLJzb0yxIZD8enr", 
+    "https://drive.google.com/uc?id=1rNrEK3jIrBzWiFSwpdSJKWJH9CvsmJgr", 
+    "https://drive.google.com/uc?id=1s_S20ALJMFZRASOeE9NUOaW7UYJV_Byd", 
+    "https://drive.google.com/uc?id=1tW6Wjx4JjYDnXFf0VvzNpegUL4M7btRO", 
+    "https://drive.google.com/uc?id=1tY_fuYZpVZiCDoBF4QFbNNAHY1KpQ2M2", 
+    "https://drive.google.com/uc?id=1uhjWF6aWGgTzc39aCOsPYGKdHWzx2fgw"
+  ], // Park photos
+  [
     "https://drive.google.com/uc?id=175uioJSXu0md2baQ-XW_wz_YhYQDfcHU", 
     "https://drive.google.com/uc?id=1GDKwHlgoCSb9SMLGZLJ0B0jyj5sUGznc", 
     "https://drive.google.com/uc?id=1N6UZWMhc-N0NsRe_liEhOk_UOc3_So4s", 
@@ -8,6 +24,25 @@ const allDrawerImages = [
     "https://drive.google.com/uc?id=1mgIc8kOZ3Wr5jE6PBsaZbd6AKzfbACT1",
     "https://drive.google.com/uc?id=1sXVKDkS26eUPpz2adnH5bSYw8mr1PrJM",
 
-  ],
+  ], // NewsPaper Cuttings
+  [
+    "https://drive.google.com/uc?id=1ylhQAS39uqwXGXY2yFVM0WiGvP6yyOl6", // Justice PreetPal
+    "https://drive.google.com/uc?id=1-40dOX8YP1CxaAAUWpvg5PYuLT2XeTVK", // Roopa mishra d
+    "https://drive.google.com/uc?id=1G_plVk0XyKERJJoMCgDBjElUWrqMbKKb",
+    "https://drive.google.com/uc?id=1JR2ux1FFreSYYX4Z4TYFJiq8bdW-8YeQ", 
+    "https://drive.google.com/uc?id=1U3EuL3opvcRLORFJyZPzyt5J_tlLmnJN",
+    "https://drive.google.com/uc?id=1NizWdq2iU_gDt44NVMY6qloNCWoO8sDC", // Mamta sharms dmc 
+    "https://drive.google.com/uc?id=1IqB5FQ3hvwGa9Mv3B7mTpHxWtQBXJZ_r", //Residents
+    "https://drive.google.com/uc?id=1iIeml-YilphTPTV6bFt4a7mU8QfNAJv6", //Vaishali singh
+    "https://drive.google.com/uc?id=1mkcWfeqU2A0PQ4qIqMdhFiLX2GCd2YX-", // aashima kakkar
+  ], // Visitors
+  [
+    "https://drive.google.com/uc?id=1SLVdOuzotb6mr2fqbE6ReR56w8V4R5jq",
+    "https://drive.google.com/uc?id=1EMnOiNa1T-MVol3ONPh-sn9UYBpFbTVp",
+    "https://drive.google.com/uc?id=1904saMbhp1n0TvYUHoGR5HTgWTLCskRo",
+    "https://drive.google.com/uc?id=1-JD7EuxKEhN98wWRo0fhR5dZdpqIdM7u",
+    "https://drive.google.com/uc?id=1DNaxdIR3-MYWnqSUI2BjW1L9Ol3CbcRN",
+  ], // Events
+   // playGrounds
 
 ];
